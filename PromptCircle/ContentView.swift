36b833f9a0ee259b077abd41abcedf206e14960b
Karan Kumar <thekarankumar.dev@gmail.com>
2025-10-06 13:07:20 +0530

    Initial Commit

---
+++ PromptCircle/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PromptCircle
+//
+//  Created by Karan Kumar on 06/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PromptCircle/PromptCircleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptCircleApp.swift
+//  PromptCircle
+//
+//  Created by Karan Kumar on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct PromptCircleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
